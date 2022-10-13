574d10350ef0c47882c1918ce7a1322791609202
Fynn Nikolaus <fynn.nikolaus@yahoo.com>
2022-10-13 21:59:53 +0200

    LED test programm

--- BirdWatcher/main.cpp
+++ BirdWatcher/main.cpp
@@ -1,10 +1,5 @@
-#include <wiringPi.h>
+#include "../../../Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/VC/Linux/include/wiringPi/2.32/wiringPi.h"
 
-// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
-// we have to use BCM numbering when initializing with wiringPiSetupSys
-// when choosing a different pin number please use the BCM numbering, also
-// update the Property Pages - Build Events - Remote Post-Build Event command
-// which uses gpio export for setup for wiringPiSetupSys
 #define	LED	17
 
 int main(void)
